f6c0d99a7a80f11c489bbcd166a0bd3fc0b683c5
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-04-10 01:31:17 +0200

    Add homework_08_04_Map

---
+++ src/main/java/de/telran/homework_08_04_Map_Hash/Arrays.java
@@ -0,0 +1,57 @@
+package de.telran.homework_08_04_Map_Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] array1 = {"Hello", "!!!", "Hello", "!!!", "!!!", "world", "world", "Hello"};
+        String[] array2 = {"Enjoy", "Hello","world", "world", "Hello", "world", "!"};
+        String[] array3 = {"world", "Hello","Enjoy",  "!", "Hello", "world", "world"};
+        System.out.println(getElemKTimes(array1, 3));
+        System.out.println("------------------");
+        System.out.println(convertInMap(array1));
+        System.out.println(convertInMap(array2));
+        System.out.println(compareArrays(array1, array2));
+        System.out.println("------------------");
+        System.out.println(convertInMap(array2));
+        System.out.println(convertInMap(array3));
+        System.out.println(compareArrays(array2, array3));
+
+    }
+
+
+    private static String getElemKTimes(String[] array, int k) {
+        Map<String, Integer> countElemMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            String elem = array[i];
+            if (!countElemMap.containsKey(elem)) {
+                countElemMap.put(elem, 1);
+            } else {
+                Integer count = countElemMap.get(elem);
+                countElemMap.put(elem, count + 1);
+                if (countElemMap.get(elem) == 3) return elem;
+            }
+        } return null;
+    }
+
+
+    private static boolean compareArrays(String[] arr1, String[] arr2) {
+        if (convertInMap(arr1).equals(convertInMap(arr2))) return true;
+        else return false;
+    }
+
+    private static Map<String, Integer> convertInMap(String[] arr) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            String elem = arr[i];
+            if (!map.containsKey(elem)) {
+                map.put(elem, 1);
+            } else {
+                Integer counter = map.get(elem);
+                map.put(elem, counter + 1);
+            }
+        } return map;
+    }
+}
+
--- src/main/java/de/telran/homework_08_04_Map_Hash/Ticket.java
+++ src/main/java/de/telran/homework_08_04_Map_Hash/Ticket.java
@@ -1,6 +1,9 @@
-package de.telran.lection13;
+package de.telran.homework_08_04_Map_Hash;
 
 import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Ticket {
 
@@ -25,7 +28,11 @@ public class Ticket {
         Ticket ticket2 = new Ticket("Tokyo", "Seoul");
         Ticket ticket3 = new Ticket("Mumbai", "Berlin");
         Ticket ticket4 = new Ticket("Seoul", "Mumbai");
-        List<Ticket> ticketList = Arrays.asList(ticket1, ticket2, ticket3, ticket4);
+        List<Ticket> ticketList = new ArrayList<>();
+        ticketList.add(ticket1);
+        ticketList.add(ticket2);
+        ticketList.add(ticket3);
+        ticketList.add(ticket4);
 
         printRoute(ticketList);
 
@@ -33,22 +40,28 @@ public class Ticket {
 
     public static void printRoute(List<Ticket> ticketList) {
         Map<String, String> ticketMap = new HashMap<>();
-        for (Ticket ticket: ticketList) {
+        for (Ticket ticket : ticketList) {
             ticketMap.put(ticket.departure, ticket.destination);
         }
+//        System.out.println(ticketMap);
         String city = ticketList.get(0).departure;
         while (ticketMap.containsKey(city)) {
             city = ticketMap.get(city);
         }
-        System.out.println(city);
-
         Map<String, String> roteMap = new HashMap<>();
-        for (Ticket ticket: ticketList) {
-            ticketMap.put(ticket.destination, ticket.departure);
+        for (Ticket ticket : ticketList) {
+            roteMap.put(ticket.destination, ticket.departure);
         }
+//        System.out.println(roteMap);
+//        System.out.println(city);
         while (roteMap.containsKey(city)) {
             city = roteMap.get(city);
-            System.out.println(city);
+
+//            System.out.println(city);
+        }
+        while (ticketMap.containsKey(city)) {
+            System.out.println(city + " -- " + ticketMap.get(city));
+            city = ticketMap.get(city);
         }
     }
 }
