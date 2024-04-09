e0deb66f8e9c2f30f21a0adc18cedd024609011f
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-04-09 11:25:48 +0200

    Update homework2_algorithms

--- src/main/java/de/telran/Algorithms/homework2/homework2.java
+++ src/main/java/de/telran/Algorithms/homework2/homework2.java
@@ -8,18 +8,21 @@ public class homework2 {
 
         int[] arr1 = {11, 22, 44, 50, 60, 86, 114, 140, 145, 190};
         int[] arr2 = {1, 24, 30, 46, 60, 100, 120, 133, 270};
-        System.out.println(findElement(arr1, 114, 0, arr1.length - 1));
-        System.out.println(findElement(arr2, 114, 0, arr1.length - 1));
+        System.out.println(findElement(arr1, 114));
+        System.out.println(findElement(arr2, 114));
 
         int[] arr3 = {4, 7, 9, 2, 15, 14};
-        System.out.println(findMax(arr3, 0, arr3[0]));
+        System.out.println(getResultTask2(arr3));
 
         Integer[] coins1 = {3, 2, 10, 2, 3};
-        System.out.println(getResultTask3(coins1, 0, 16));
+        System.out.println(getResultTask3(coins1, 16));
     }
 
 
-    public static int findElement(int[] arr, int x, int indexStart, int indexEnd) {
+    public static int findElement(int[] arr, int x) {
+        return serchElem(arr, x, 0, arr.length - 1);
+    }
+    public static int serchElem(int[] arr, int x, int indexStart, int indexEnd) {
         int indexMiddle = indexStart + (indexEnd - indexStart) / 2;
         if (indexEnd < indexStart) {
             return -1;
@@ -28,13 +31,16 @@ public class homework2 {
             return indexMiddle;
         } else if (arr[indexMiddle] < x) {
             indexStart = indexMiddle + 1;
-            return findElement(arr, x, indexStart, indexEnd);
+            return serchElem(arr, x, indexStart, indexEnd);
         } else {
             indexEnd = indexMiddle - 1;
-            return findElement(arr, x, indexStart, indexEnd);
+            return serchElem(arr, x, indexStart, indexEnd);
         }
     }
 
+    private static int getResultTask2(int[] arr) {
+        return findMax(arr, 1, arr[0]);
+    }
     private static int findMax(int[] arr, int i, int max) {
         if (i == arr.length) {
             return max;
@@ -63,8 +69,10 @@ public class homework2 {
     }
 
 
-    private static boolean getResultTask3(Integer[] coins, int i, int x) {
+    private static boolean getResultTask3(Integer[] coins, int x) {
         Arrays.sort(coins, Collections.reverseOrder());
-        return canPay(coins, i, x);
+        return canPay(coins, 0, x);
     }
+//    здесь без сортировки я не нашла вариант решения,
+//    т к нужно же сначала монеты большего номинала использовать, а пртом уже добирать меньшими
 }
